50dad15da5011b2111730743db2543b61e995846
Zethus <ravet.zvika93@gmail.com>
2021-03-15 23:23:03 +0200

    check for Microsoft Office installation

--- Nyaa-Downloader.ps1
+++ Nyaa-Downloader.ps1
@@ -51,3 +51,20 @@ Param
 # ~~~~~~~~~~~~~~~~~~~~~ Don't Touch ~~~~~~~~~~~~~~~~~~~~ #
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
+$ErrorActionPreference = "Stop"
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Check if Microsoft Office is intalled
+
+if(!(Get-ItemProperty 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*','HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*' | `
+   ? {$_.Publisher -eq "Microsoft Corporation" -and ($_.DisplayName -match "Microsoft Office" -or $_.DisplayName -match "Microsoft 365")}))
+{
+
+    Write-Host "[     " -NoNewline -ForegroundColor Red
+    Write-Host "ERROR" -NoNewline -ForegroundColor Red -BackgroundColor Black
+    Write-Host "     ] " -NoNewline -ForegroundColor Red
+    Write-Host "Didn't find an installation of Microsoft Office" -ForegroundColor Red
+
+    pause
+    break
+}
